656c4969f73f1f4a50176439068fc163ea5106d8
wang0805 <wh_wang@live.com>
2020-09-14 22:14:16 +0800

    changed blue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import {Route, Switch} from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Page from "react-page-loading";
 
 //Package CSS & JS
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "animate.css/animate.min.css";
 
@@ -19,26 +19,16 @@ import HomeThree from "./pages/HomeThree";
 class App extends Component {
   render() {
     return (
-        <div className="App">
-            <div>
-                <Page loader={"bubble-spin"} color={"#71c21b"} size={9}>
-                    <Switch >
-                    <Route
-                        path="/home-three"
-                        component={HomeThree}
-                    />/
-                    
-                    <Route
-                        path="/home-two"
-                        component={HomeTwo}
-                    />
-                    <Route
-                        path="/"
-                        component={HomeOne}
-                    />
-                    </Switch>
-                </Page>
-            </div>
+      <div className="App">
+        <div>
+          <Page loader={"bubble-spin"} color={"#282560"} size={9}>
+            <Switch>
+              <Route path="/home-three" component={HomeThree} />/
+              <Route path="/home-two" component={HomeTwo} />
+              <Route path="/" component={HomeOne} />
+            </Switch>
+          </Page>
+        </div>
       </div>
     );
   }
